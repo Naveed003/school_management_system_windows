b4e8c287eff9c576f3f23ebbb92b07f9f5a6d07f
naveed <imnaveed2003@gmail.com>
2020-07-09 14:36:36 +0400

    added

--- GUIs/hello.py
+++ GUIs/hello.py
@@ -13,17 +13,17 @@ files = [
     "DELETE_BOOK",
     "statistics",
     "issue_book",
-    "return_book"
+    "return_book",
+    "ABOUT"
 ]
 cmd = "pyuic5 -x "
 
 for i in files:
-    if i=="student_data":
-        i=i.upper()
-        temp = cmd +"GUIs/"+ i + ".ui -o  PROGRAMS/" + i + ".py"
-        print(temp)
-        os.system(temp)
-        temp = ''
+    i=i.upper()
+    temp = cmd +"GUIs/"+ i + ".ui -o  PROGRAMS/" + i + ".py"
+    print(temp)
+    os.system(temp)
+    temp = ''
 
 #QToolButton(background-color:#33ff39;border:2px;border-radius: 8px;COLOR: BLACK;"
 #msgbox.setStyleSheet("background-color: rgb(8,8,8);color:white;font-family:Times New Roman;QLabel{ color: white} QMessageBox QPushButton{background-color: Silver;} ")
