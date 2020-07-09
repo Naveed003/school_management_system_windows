9580aefb68acc55bb19a916198b4bfa37d4a6886
naveed win <imnaveed2003@gmail.com>
2020-07-09 11:31:04 +0400

    added

---
+++ GUIs/hello.py
@@ -0,0 +1,29 @@
+import os
+files = [
+    "admission_menu",
+    "fee_deposit",
+    "fee_menu",
+    "LOGIN_PAGE",
+    "main_menu",
+    "new_user_page",
+    "student_data",
+    "ADD_BOOK",
+    "library_menu",
+    "fee_details",
+    "DELETE_BOOK",
+    "statistics",
+    "issue_book",
+    "return_book"
+]
+cmd = "pyuic5 -x "
+
+for i in files:
+    if i=="student_data":
+        i=i.upper()
+        temp = cmd +"GUIs/"+ i + ".ui -o  PROGRAMS/" + i + ".py"
+        print(temp)
+        os.system(temp)
+        temp = ''
+
+#QToolButton(background-color:#33ff39;border:2px;border-radius: 8px;COLOR: BLACK;"
+#msgbox.setStyleSheet("background-color: rgb(8,8,8);color:white;font-family:Times New Roman;QLabel{ color: white} QMessageBox QPushButton{background-color: Silver;} ")
